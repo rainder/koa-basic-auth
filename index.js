2bc5519d568add6760c09becccca2a866d2345ef
Andrius Skerla <andrius@skerla.com>
2016-11-29 10:31:44 +0000

    add password hashing method

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 'use strict';
 
 const basicAuth = require('basic-auth');
+const crypto = require('crypto');
 
-module.exports = function (credentials = {}) {
+module.exports = function (credentials = {}, options = {}) {
   return function *(next) {
     const user = basicAuth(this);
+
+    if (options.hash) {
+      user.pass = crypto.createHash(options.hash).update(user.pass).digest('hex');
+      console.log(user.pass);
+    }
 
     if (
       user &&
